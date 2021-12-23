5f7a95d9b71301fa229fd916441df07d4572613e
Yunyor <allanacan@gmail.com>
2021-12-22 18:50:55 -0600

    se ha arreglado responsive

--- resposive.js
+++ resposive.js
@@ -3,9 +3,7 @@ $(document).ready(main);
 var contador = 0;
 var bandera =0;
 function main () {
-	$('nav').animate({
-		left: '-100%'
-	});
+	
 	$('.menu_bar').click(function(){
 		if (contador == 1) {
 			$('nav').animate({
