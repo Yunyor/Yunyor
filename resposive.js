b2805e502794aad52b8ec08866a00aebce330c10
Yunyor <allanacan@gmail.com>
2021-12-22 18:45:09 -0600

    Se ha agregado responsive en menu mobile

--- resposive.js
+++ resposive.js
@@ -1,8 +1,11 @@
 $(document).ready(main);
 
-var contador = 1;
-
+var contador = 0;
+var bandera =0;
 function main () {
+	$('nav').animate({
+		left: '-100%'
+	});
 	$('.menu_bar').click(function(){
 		if (contador == 1) {
 			$('nav').animate({
@@ -17,8 +20,6 @@ function main () {
 		}
 	});
 
-	// Mostramos y ocultamos submenus
-	$('.submenu').click(function(){
-		$(this).children('.children').slideToggle();
-	});
+	
+
 }
